d8c3811c960be59fa18fa12ce479345be5295ad5
Sye van der Veen <syeberman@rogers.com>
2012-09-16 20:41:10 -0500

    Fixing the "NULL on success" silliness, new ideas on freezing/unfreezing errors, updating the convenience macros, better bytes coersion, etc.
    [git-p4: depot-paths = "//depot/main/nohtyP/": change = 60]

--- nohtyP.c
+++ nohtyP.c
@@ -84,9 +84,7 @@ typedef struct {
     unaryfunc nb_int;
     unaryfunc nb_float;
     unaryfunc nb_index;
-} ypNumberMethods;
 
-typedef struct {
     binaryfunc nb_iadd;
     binaryfunc nb_isubtract;
     binaryfunc nb_imultiply;
@@ -104,19 +102,17 @@ typedef struct {
     binaryfunc nb_ior;
     binaryfunc nb_ifloor_divide;
     binaryfunc nb_itrue_divide;
-} ypMutableNumberMethods;
+} ypNumberMethods;
 
 
 typedef struct {
     binaryfunc sq_concat;
     ssizeargfunc sq_repeat;
-} ypSequenceMethods;
 
-typedef struct {
-    binaryfunc sq_iconcat;
+    binaryfunc sq_extend;
     ssizeargfunc sq_irepeat;
-    // TODO iappend et al
-} ypMutableSequenceMethods;
+    // TODO append et al
+} ypSequenceMethods;
 
 
 
@@ -155,11 +151,8 @@ typedef struct {
 
     /* Method suites for standard classes */
     ypNumberMethods *tp_as_number;
-    ypMutableNumberMethods *tp_as_mutable_number;
     ypSequenceMethods *tp_as_sequence;
-    ypMutableSequenceMethods *tp_as_mutable_sequence;
     ypMappingMethods *tp_as_mapping;
-    ypMutableMappingMethods *tp_as_mutable_mapping;
 
     ypObject *tp_doc; /* Documentation string */
 
@@ -183,7 +176,7 @@ typedef struct {
 #define ypNone_CODE                 (  6u)
 // no mutable ypNone type           (  7u)
 #define ypBool_CODE                 (  8u)
-// TODO what to call a mutalbe bool (  9u)
+// TODO what to call a mutable bool (  9u)
 
 #define ypInt_CODE                  ( 10u)
 // TODO what to call a mutable int  ( 11u)
@@ -267,7 +260,7 @@ typedef struct {
         (ob)->ob_alloclen = alloclen; \
     } while( 0 )
 
-// Resizes ob_data, the variable-portion of ob, to the given alloclen, and sets result to NULL.
+// Resizes ob_data, the variable-portion of ob, to the given alloclen, and sets result to yp_None.
 // If realloc fails, result is set to yp_MemoryError and ob is not modified.  ob_inline_data in
 // obStruct is used to determine the element size; ob_alloclen is set to newAlloclen.
 // Any objects in the  truncated section must have already been discarded.  newAlloclen cannot be
@@ -277,7 +270,7 @@ typedef struct {
         void *newData = realloc( \
                 (newAlloclen) * _yp_sizeof_member( obStruct, ob_inline_data[0] ) ); \
         if( newData == NULL ) { (result) = yp_MemoryError; break; }; \
-        (result) = NULL; /* success */ \
+        (result) = yp_None; \
         (ob)->ob_data = newData; \
         (ob)->ob_alloclen = newAlloclen; \
     } while( 0 )
@@ -348,20 +341,20 @@ ypObject *_yp_freeze( ypObject *x )
     ypObject *result;
 
     // Check if it's already frozen (no-op) or if it can't be frozen (error)
-    if( oldCode == newCode ) return NULL;
+    if( oldCode == newCode ) return yp_None;
     newType = ypTypeTable[newCode];
-    if( newType == NULL ) return yp_TypeError;
+    if( newType == NULL ) return yp_TypeError;  // TODO return a new, invalidated object
 
-    // Freeze the object, ensure hash will be recalculated, and possibly reduce memory usage, etc
+    // Freeze the object, cache the final hash, and possibly reduce memory usage, etc
     ypObject_SET_TYPE_CODE( *x, newCode );
-    (*x)->ob_hash = ypObject_HASH_NOT_CACHED;
+    (*x)->ob_hash = newType->tp_current_hash( *x ); // TODO rename?
     return newType->tp_after_freeze( *x );
 }
 
 void yp_freeze( ypObject **x )
 {
     ypObject *result = _yp_freeze( *x );
-    if( result != NULL ) return_yp_INPLACE_ERR( *x, result );
+    if( yp_isexceptionC( result ) ) return_yp_INPLACE_ERR( *x, result );
 }
 
 ypObject *_yp_deepfreeze( ypObject *x, ypObject *memo )
@@ -370,17 +363,17 @@ ypObject *_yp_deepfreeze( ypObject *x, ypObject *memo )
     ypObject *result;
     
     // Avoid recursion
-    // TODO yp_add_unique returns key error if object already in set
+    // TODO yp_addunique returns key error if object already in set
     id = yp_intC( (yp_int64_t) x );
-    result = yp_add_unique( memo, id );
+    result = yp_addunique( memo, id );
     yp_decref( id );
-    if( result == yp_KeyError ) return NULL; // success
+    if( result == yp_KeyError ) return yp_None;
     if( yp_isexceptionC( result ) ) return result;
 
     // Freeze current object before going deep
     result = _yp_freeze( x );
-    if( result != NULL ) return result;
-    // TODO tp_traverse should return if its visitor returns non-NULL...?  Or should we track
+    if( yp_isexceptionC( result ) ) return result;
+    // TODO tp_traverse should return if its visitor returns exception...?  Or should we track
     // additional data in what is currently memo?
     return ypObject_TYPE( x )->tp_traverse( x, _yp_deepfreeze, memo );
 }
@@ -390,25 +383,19 @@ void yp_deepfreeze( ypObject **x )
     ypObject *memo = yp_setN( 0 );
     ypObject *result = _yp_deepfreeze( *x, memo );
     yp_decref( memo );
-    if( result != NULL ) return_yp_INPLACE_ERR( *x, result );
+    if( yp_isexceptionC( result ) ) return_yp_INPLACE_ERR( *x, result );
 }
 
-// Returns a new reference to a mutable shallow copy of x, or yp_MemoryError.
 ypObject *yp_unfrozen_copy( ypObject *x );
 
-// Returns a new reference to a mutable deep copy of x, or yp_MemoryError.
 ypObject *yp_unfrozen_deepcopy( ypObject *x );
 
 ypObject *yp_frozen_copy( ypObject *x );
 
 ypObject *yp_frozen_deepcopy( ypObject *x );
 
-// Steals x, discards all referenced objects, deallocates _some_ memory, transmutes it to
-// the ypInvalidated type (rendering the object useless), and returns a new reference to x.
-// If x is immortal or already invalidated this is a no-op.
 void yp_invalidate( ypObject **x );
 
-// Steals and invalidates x and, recursively, all referenced objects, returning a new reference.
 void yp_deepinvalidate( ypObject **x );
 
 
@@ -438,15 +425,12 @@ void yp_deepinvalidate( ypObject **x );
     if( type->tp_suite == NULL || type->tp_suite->suite_meth == NULL ) return ypMethodError; \
     return type->tp_suite->suite_meth args;
 
-// TODO In-place methods need a way to signal that the given object should not be replaced; could
-// either return the object (without having incref'd, although any other reference would need to
-// be, which is inconsistent!) or can return NULL...let's go with: "NULL, or a new reference"
 #define _yp_INPLACE1( pOb, tp_meth, args ) \
     ypTypeObject *type = ypObject_TYPE( *pOb ); \
     ypObject *result; \
     if( type->tp_meth == NULL ) result = ypMethodError; \
     else result = type->tp_meth args; \
-    if( result != NULL ) return_yp_INPLACE_ERR( *pOb, result ); \
+    if( yp_isexceptionC( result ) ) return_yp_INPLACE_ERR( *pOb, result ); \
     return;
 
 #define _yp_INPLACE2( pOb, tp_suite, suite_meth, args ) \
@@ -454,11 +438,11 @@ void yp_deepinvalidate( ypObject **x );
     ypObject *result; \
     if( type->tp_suite == NULL || type->tp_suite->suite_meth == NULL ) result = ypMethodError; \
     else result = type->tp_suite->suite_meth args; \
-    if( result != NULL ) return_yp_INPLACE_ERR( *pOb, result ); \
+    if( yp_isexceptionC( result ) ) return_yp_INPLACE_ERR( *pOb, result ); \
     return;
 
 void yp_append( ypObject **s, ypObject *x ) {
-    _yp_INPLACE2( s, tp_as_mutable_sequence, sq_iappend, (*s, x) )
+    _yp_INPLACE2( s, tp_as_sequence, sq_append, (*s, x) )
 }
 
 
@@ -466,10 +450,22 @@ void yp_append( ypObject **s, ypObject *x ) {
 
 // TODO undef necessary stuff
 
+/*************************************************************************************************
+ * Invalidated Objects
+ *************************************************************************************************/
+
+
+
+/*************************************************************************************************
+ * Exceptions
+ *************************************************************************************************/
+
 
 /*************************************************************************************************
  * Bools
  *************************************************************************************************/
+
+// TODO: A "ypSmallObject" type for type codes < 8, say, to avoid wasting space for bool/int/float?
 
 // Returns 1 if the bool object is true, else 0; only valid on bool objects!  The return can also
 // be interpreted as the value of the boolean.
@@ -480,6 +476,8 @@ void yp_append( ypObject **s, ypObject *x ) {
 /*************************************************************************************************
  * Integers
  *************************************************************************************************/
+
+// TODO: A "ypSmallObject" type for type codes < 8, say, to avoid wasting space for bool/int/float?
 
 static ypObject *yp_int_iadd( ypObject *x, ypObject *y ) 
 {
@@ -497,6 +495,9 @@ static ypObject *yp_int_add( ypObject *x, ypObject *y )
 /*************************************************************************************************
  * Floats
  *************************************************************************************************/
+
+// TODO: A "ypSmallObject" type for type codes < 8, say, to avoid wasting space for bool/int/float?
+
 
 
 /*************************************************************************************************
@@ -556,61 +557,86 @@ static ypObject *_yp_bytes_copy( ypObject *b, yp_ssize_t len )
     return newB;
 }
 
-// Shrinks or grows the bytearray; any new bytes are uninitialized.  Returns NULL on success,
+// Shrinks or grows the bytearray; any new bytes are uninitialized.  Returns yp_None on success,
 // exception on error.
 static ypObject *_yp_bytearray_resize( ypObject *b, yp_ssize_t newLen )
 {
     ypObject *result;
     ypMem_REALLOC_CONTAINER_VARIABLE( result, *b, ypBytesObject, newLen );
-    if( result != NULL ) return result;
+    if( yp_isexceptionC( result ) ) return result;
     b->ob_len = len;
-    return NULL;
+    return yp_None;
 }
 
-// If x is an exception, return immediately.  If x is a bool/int in range(256), store value in
-// storage and set *x_data=storage, x_len=1.  If x is a fellow bytes, set *x_data and x_len.
-// Returns NULL on success, exception on error.
-static ypObject *_bytes_coerce( ypObject *x, yp_uint8_t **x_data, yp_ssize_t *x_len, 
+// If x is a fellow bytes, set *x_data and x_len.  Otherwise, set *x_data=NULL and *x_len=0.
+static void _bytes_coerce_bytes( ypObject *x, yp_uint8_t **x_data, yp_ssize_t *x_len )
+{
+    ypObject *result;
+    int x_type = yp_TYPE_PAIR_CODE( x );
+
+    if( x_type == ypBytes_CODE ) {
+        *x_data = ypBytes_DATA( x );
+        *x_len = ypBytes_LEN( x );
+        return;
+    }
+    
+    *x_data = NULL;
+    *x_len = 0;
+    return;
+}
+
+// If x is a bool/int in range(256), store value in storage and set *x_data=storage, x_len=1.  If 
+// x is a fellow bytes, set *x_data and x_len.  Otherwise, set *x_data=NULL and *x_len=0.
+static void _bytes_coerce_intorbytes( ypObject *x, yp_uint8_t **x_data, yp_ssize_t *x_len, 
         yp_uint8_t *storage )
 {
     ypObject *result;
     int x_type = yp_TYPE_PAIR_CODE( x );
-    if( x_type == ypException_CODE ) return x;
 
-    if( x_type == yp_Bool_CODE || x_type == ypInt_CODE ) {
+    if( x_type == ypBool_CODE || x_type == ypInt_CODE ) {
         result = yp_as_uint8C( x, storage );
-        if( yp_isexceptionC( result ) ) return result;
-        *x_data = storage;
-        *x_len = 1;
+        if( !yp_isexceptionC( result ) ) {
+            *x_data = storage;
+            *x_len = 1;
+            return;
+        }
     } else if( x_type == ypBytes_CODE ) {
         *x_data = ypBytes_DATA( x );
         *x_len = ypBytes_LEN( x );
-    } else {
-        return yp_TypeError;
+        return;
     }
-    return NULL; // success
+
+    *x_data = NULL;
+    *x_len = 0;
+    return;
 }
 
-// Returns NULL or an exception
-// XXX Ensure that yp_ValueError from this function unambiguously means "not found"
+// TODO Returns yp_None or an exception
+// static ypObject *bytes_index3_asC( ypObject *b, ypObject *x, yp_ssize_t start, yp_ssize_t end, yp_ssize_t *i );
+// TODO for naming convention, how to convey that x is nohtyP, but start/end is C?
+
+// TODO similarly, a yp_find that returns -1
+
+// Returns yp_None or an exception
+// XXX Careful, yp_ValueError also returned if x is yp_ValueError
 static ypObject *bytes_index_asC( ypObject *b, ypObject *x, yp_ssize_t *i )
 {
-    ypObject *result;
     yp_uint8_t *x_data;
     yp_ssize_t x_len;        
     yp_uint8_t storage;
     yp_uint8_t *b_rdata;   // remaining data
     yp_ssize_t b_rlen;     // remaining length
     
-    result = _bytes_coerce( x, &x_data, &x_len, &storage );
-    if( result != NULL ) return result;
+    if( isexceptionC( x ) ) return x;
+    _bytes_coerce_intorbytes( x, &x_data, &x_len, &storage );
+    if( x_data == NULL ) return yp_ValueError;
 
     b_rdata = ypBytes_DATA( b );
     b_rlen = ypBytes_LEN( b );
     while( b_rlen >= x_len ) {
         if( memcmp( b_rdata, x_data, x_len ) == 0 ) {
             *i = b_rdata - ypBytes_DATA( b );
-            return NULL; // success
+            return yp_None;
         }
         b_rdata++; b_rlen--;
     }
@@ -622,8 +648,10 @@ static ypObject *bytes_contains( ypObject *b, ypObject *x )
 {
     ypObject *result;
     yp_ssize_t i = -1;
+
+    if( isexceptionC( x ) ) return x;
     result = bytes_index_asC( b, x, &i );
-    if( result == NULL ) return yp_True; // item found
+    if( result == yp_None ) return yp_True; // item found
     if( result == yp_valueError ) return yp_False; // item not found
     return result; // error
 }
@@ -631,14 +659,14 @@ static ypObject *bytes_contains( ypObject *b, ypObject *x )
 // Returns new reference or an exception
 static ypObject *bytes_concat( ypObject *b, ypObject *x )
 {
-    ypObject *result;
     yp_uint8_t *x_data;
     yp_ssize_t x_len;        
     yp_uint8_t storage;
     ypObject *newB;
     
-    result = _bytes_coerce( x, &x_data, &x_len, &storage );
-    if( result != NULL ) return result;
+    if( yp_isexceptionC( x ) ) return x;
+    _bytes_coerce_bytes( x, &x_data, &x_len, &storage );
+    if( x_data == NULL ) return yp_TypeError;
 
     newB = _yp_bytes_copy( b, ypBytes_LEN( b ) + x_len );
     if( isexceptionC( newB ) ) return newB;
@@ -647,28 +675,29 @@ static ypObject *bytes_concat( ypObject *b, ypObject *x )
     return newB;
 }
 
-// Returns NULL or an exception
-static ypObject *bytearray_iconcat( ypObject *b, ypObject *x )
+// Returns yp_None or an exception
+static ypObject *bytearray_extend( ypObject *b, ypObject *x )
 {
-    ypObject *result;
     yp_uint8_t *x_data;
     yp_ssize_t x_len;        
     yp_uint8_t storage;
+    ypObject *result;
     
-    result = _bytes_coerce( x, &x_data, &x_len, &storage );
-    if( result != NULL ) return result;
+    if( yp_isexceptionC( x ) ) return x;
+    _bytes_coerce( x, &x_data, &x_len, &storage );
+    if( x_data == NULL ) return yp_TypeError;
 
     result = _yp_bytearray_resize( b, ypBytes_LEN( b ) + x_len );
-    if( result != NULL ) return result;
+    if( yp_isexceptionC( result ) ) return result;
 
     memcpy( ypBytes_DATA( b )+ypBytes_LEN( b ), x_data, x_len );
-    return NULL;
+    return yp_None;
 }
 
 // TODO bytes_repeat
 
 // Returns new reference or an exception
-static ypObject *bytes_getitem_seqC( ypObject *b, yp_ssize_t i )
+static ypObject *bytes_getindexC( ypObject *b, yp_ssize_t i )
 {
     if( i < 0 ) i += ypBytes_LEN( b );
     if( i < 0 || i >= ypBytes_LEN( b ) ) return yp_IndexError;
@@ -682,13 +711,13 @@ static ypObject *bytes_getsliceC( ypObject *b, yp_ssize_t start, yp_ssize_t stop
 }
 
 // Returns new reference or an exception
-// TODO bytes_getitem...need a generic function to redirect to getitem_seqC or getsliceC
+// TODO bytes_getitem...need a generic function to redirect to getindexC or getsliceC
 
-// Returns NULL or an exception
+// Returns yp_None or an exception
 static ypObject *bytes_lenC( ypObject *b, yp_ssize_t *len )
 {
     *len = ypBytes_LEN( b );
-    return NULL;
+    return yp_None;
 }
 
 // bytes_index_asC is above
