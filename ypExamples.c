e46de57c28d45bf51a6781d9edac933d10164ce8
Sye van der Veen <4551516+Syeberman@users.noreply.github.com>
2012-12-23 15:45:22 -0500

    The current ConditionalStatements examples all succeed; now working on a set example.
    [git-p4: depot-paths = "//depot/main/nohtyP/": change = 91]

--- ypExamples.c
+++ ypExamples.c
@@ -1,5 +1,5 @@
 /*
- * ypExamples.c - Contains a number of nohtyP examples and recipies
+ * ypExamples.c - Contains a number of nohtyP examples
  *      http://nohtyp.wordpress.com
  *      Copyright © 2001-2012 Python Software Foundation; All Rights Reserved
  *      License: http://docs.python.org/py3k/license.html
@@ -20,28 +20,28 @@ typedef int bool;
 #define false 0
 #define true 1
 
-#define RECIPIE( name ) do { ypRecipies_recipie_name = #name; printf( "\nRecipie %s\n", ypRecipies_recipie_name ); _flushall( );} while(0);
+#define EXAMPLE( group, name ) do { ypExamples_example_name = #group #name; printf( "\nExample %s\n", ypExamples_example_name ); _flushall( );} while(0);
 #define PrintError( fmt, ... ) fprintf( stderr, "  line %d: " fmt "\n", __LINE__, __VA_ARGS__ )
-#define Expect( cond, fmt, ... ) do { if( cond ) break; ypRecipies_result++; PrintError( fmt, __VA_ARGS__ ); } while(0)
+#define Expect( cond, fmt, ... ) do { if( cond ) break; ypExamples_result++; PrintError( fmt, __VA_ARGS__ ); } while(0)
 // TODO include exception name
 #define ExpectTrue( act ) Expect( act, #act " is not true", 0 )
 #define ExpectUnreachable( ) Expect( false, "unexpectedly reachable code", 0 )
 #define ExpectEqual( exp, act ) Expect( (exp) == (act), #exp " != " #act, 0 )
 #define ExpectNoException( obj ) Expect( !yp_isexceptionC( obj ), "unexpected exception (0x%08X)", (obj) )
 
-int ypRecipies( void )
+int ypExamples( void )
 {
-    int ypRecipies_result = 0; // return value from ypRecipies: number of failures
-    char *ypRecipies_recipie_name = ""; // current recipie name TODO needed?
+    int ypExamples_result = 0; // return value from ypExamples: number of failures
+    char *ypExamples_example_name = ""; // current example name TODO needed?
 
 
 /*
  * Building lists
  */
 
-/* Exceptions propagate: when given as an input they are returned immediately.  This recipie shows
+/* Exceptions propagate: when given as an input they are returned immediately.  This example shows
  * that when building a list, you are free to ignore exceptions until the end. */
-RECIPIE( BuildingLists_DiscardOnException )
+EXAMPLE( BuildingLists, DiscardOnException )
 {
     ypObject *list = yp_listN( 0 );
     int i;
@@ -72,7 +72,7 @@ RECIPIE( BuildingLists_DiscardOnException )
 
 /* Sometimes you don't want the list you're building to be discarded on error.  As demonstrated
  * below, avoiding this is simple. */
-RECIPIE( BuildingLists_KeepOnException )
+EXAMPLE( BuildingLists, KeepOnException )
 {
     ypObject *list = yp_listN( 0 );
     ypObject *item;
@@ -108,13 +108,104 @@ RECIPIE( BuildingLists_KeepOnException )
 
 
 /*
+ * Sets
+ */
+
+/* Taken from Python's tutorial */
+EXAMPLE( Sets, BriefDemonstration )
+{
+    // Rather than allocating at runtime, immutable objects such as bytes can be statically
+    // allocated either locally or globally
+    yp_IMMORTAL_BYTES( b_apple, "apple" );
+    yp_IMMORTAL_BYTES( b_orange, "orange" );
+    yp_IMMORTAL_BYTES( b_pear, "pear" );
+    yp_IMMORTAL_BYTES( b_banana, "banana" );
+    yp_IMMORTAL_BYTES( b_crabgrass, "crabgrass" );
+    yp_IMMORTAL_BYTES( b_abracadabra, "abracadabra" );
+    yp_IMMORTAL_BYTES( b_alacazam, "alacazam" );
+    yp_IMMORTAL_BYTES( b_a, "a" );
+    yp_IMMORTAL_BYTES( b_b, "b" );
+    yp_IMMORTAL_BYTES( b_z, "z" );
+    yp_IMMORTAL_BYTES( b_q, "q" );
+
+    /* yp_lenC and similar functions only modify exc on error; since they do not discard existing
+     * values, exc should be initialized to yp_None or another immortal. */
+    ypObject *exc = yp_None;
+
+    {
+        /* Sets can be initialized using a variable-number of object arguments */
+        ypObject *basket = yp_setN( 6, b_apple, b_orange, b_apple, b_pear, b_orange, b_banana );
+
+        /* Duplicates are removed from sets: of the 6 objects supplied to yp_setN, only 4 are
+         * unique */
+        ExpectEqual( 4, yp_lenC( basket, &exc ) );
+
+        /* Sets support fast membership testing.  Also of note: there are exactly two bool objects,
+         * yp_True and yp_False, so comparing equality of object pointers is perfectly valid. */
+        ExpectEqual( yp_True, yp_in( b_orange, basket ) );
+        ExpectEqual( yp_False, yp_in( b_crabgrass, basket ) );
+
+        /* Don't forget to clean-up the new reference returned by yp_setN! */
+        yp_decref( basket );
+    }
+    {
+        /* Sets can also be initialized from other iterable objects */
+        ypObject *a = yp_set( b_abracadabra );
+        ypObject *b = yp_set( b_alacazam );
+        ypObject *result;
+
+        /* Unique letters in a: {'a', 'r', 'b', 'c', 'd'} */
+        ExpectEqual( 5, yp_lenC( a, &exc ) );
+        ExpectEqual( yp_True, yp_contains( b_a, a ) );
+        ExpectEqual( yp_False, yp_contains( b_z, a ) );
+
+        /* Letters in a but not in b: {'r', 'd', 'b'}
+         * If you forget that yp_differenceN returns a new reference, you'll leak some memory */
+        result = yp_differenceN( a, 1, b );
+        ExpectEqual( 3, yp_lenC( result, &exc ) );
+        ExpectEqual( yp_True, yp_contains( b_b, result ) );
+        ExpectEqual( yp_False, yp_contains( b_a, result ) );
+        yp_decref( result );
+
+        /* Letters in either a or b: {'a', 'c', 'r', 'd', 'b', 'm', 'z', 'l'} */
+        result = yp_unionN( a, 1, b );
+        ExpectEqual( 8, yp_lenC( result, &exc ) );
+        ExpectEqual( yp_True, yp_contains( b_z, result ) );
+        ExpectEqual( yp_False, yp_contains( b_q, result ) );
+        yp_decref( result );
+
+        /* Letters in both a and b: {'a', 'c'} */
+        result = yp_intersectionN( a, 1, b );
+        ExpectEqual( 2, yp_lenC( result, &exc ) );
+        ExpectEqual( yp_True, yp_contains( b_a, result ) );
+        ExpectEqual( yp_False, yp_contains( b_b, result ) );
+        yp_decref( result );
+
+        /* Letters in a or b but not both: {'r', 'd', 'b', 'm', 'z', 'l'} */
+        result = yp_symmetric_difference( a, b );
+        ExpectEqual( 6, yp_lenC( result, &exc ) );
+        ExpectEqual( yp_True, yp_contains( b_z, result ) );
+        ExpectEqual( yp_False, yp_contains( b_a, result ) );
+        yp_decref( result );
+
+        /* You can clean-up multiple references with a single command */
+        yp_decrefN( 2, a, b );
+    }
+
+    /* Immortals, including yp_None, don't need to be decref'd */
+    ExpectEqual( yp_None, exc );
+}
+
+
+
+/*
  * Conditional statements
  */
 
 /* yp_ELIF allows for chaining conditions, just as in Python.  However, yp_ELSE_EXCEPT_AS doesn't
  * work in quite the same way... */
 // FIXME looks like yp_IF isn't executing _any_ of the conditions
-RECIPIE( ConditionalStatements_ELIF )
+EXAMPLE( ConditionalStatements, ELIF )
 {
     ypObject *cond1 = yp_bytesC( NULL, 0 );
     ypObject *cond2 = yp_intC( 5 );
@@ -144,7 +235,7 @@ RECIPIE( ConditionalStatements_ELIF )
 
 /* Sometimes you'll generate a new, mortal reference in your condition statement that needs to be
  * discarded: yp_IFd handles this (the 'd' is for "discard"). */
-RECIPIE( ConditionalStatements_IFd )
+EXAMPLE( ConditionalStatements, IFd )
 {
     ypObject *bytes = yp_bytesC( "ABCDE", -1 ); // -1 means "null-terminated"
     bool branch1_taken = false;
@@ -167,7 +258,7 @@ RECIPIE( ConditionalStatements_IFd )
 }
 
 /* Here's how you handle an exception occuring in one of your condition statements. */
-RECIPIE( ConditionalStatements_EXCEPT_AS )
+EXAMPLE( ConditionalStatements, EXCEPT_AS )
 {
     ypObject *bytes = yp_bytesC( "ABCDE", -1 ); // -1 means "null-terminated"
     ypObject *e = yp_None; // good to initialize this if you plan to use it after yp_ENDIF
@@ -191,13 +282,13 @@ RECIPIE( ConditionalStatements_EXCEPT_AS )
 // (...because it's so easy to create immortals for bytes/str)
 
 
-    // End of ypRecipies
+    // End of ypExamples
     system("pause"); // FIXME why isn't VC++ pausing?
-    return ypRecipies_result;
+    return ypExamples_result;
 } 
 
 int main(int argc, char *argv[], char *envp[])
 {
     yp_initialize( );
-    return ypRecipies( ); // TODO complete
+    return ypExamples( ); // TODO complete
 }
