43ec64ff06ecff21ebf769cbb83a3454ebbca002
Sye van der Veen <4551516+Syeberman@users.noreply.github.com>
2014-01-24 15:48:31 -0500

    Finished debug- and release-specific MSVC options, and starting to fix breakages caused

--- Build/Tools/msvs_common.py
+++ Build/Tools/msvs_common.py
@@ -14,20 +14,51 @@ _mslinkTool = SCons.Tool.Tool( "mslink" )
 def ApplyMSVSOptions( env, version ):
     """Updates env with MSVS-specific compiler options for nohtyP.  version is numeric (ie 12.0).
     """
-    pass
+    def addCcFlags( *args ): env.AppendUnique( CCFLAGS=list( args ) )
+    addCcFlags(
+            # Warning level 3, warnings-as-errors, security compile- and runtime-checks
+            "/W3", "/WX", "/sdl",
+            # Function-level linking, disable minimal rebuild, disable runtime type info
+            "/Gy", "/Gm-", "/GR-",
+            # Source/assembly listing, exception handling model
+            # FIXME assembly file written to top-level directory
+            "/FAs", "/EHsc",
+            )
+    if env["CONFIGURATION"] == "debug":
+        addCcFlags( 
+                # Runtime checks, disable optimizations
+                "/RTCcsu", "/Od",
+                # Debug multithread and DLL MSVCRT
+                "/MDd",
+                )
+    else:
+        addCcFlags( 
+                # Whole program optimization, full speed optimizations, 
+                "/GL", "/Ox",
+                # Multithreaded and DLL MSVCRT
+                "/MD",
+                )
+    # Disable frame-pointer omission (ie frame pointers will be available on all builds)
+    # XXX Must come after any other optimization compiler options
+    addCcFlags( "/Oy-" )
+    # Improved debugging of optimized code: http://wp.me/p1fTCO-my
+    if version >= 12.0: addCcFlags( "/d2Zi+" )
+
+    def addCppDefines( *args ): env.AppendUnique( CPPDEFINES=list( args ) )
+    if env["CONFIGURATION"] == "debug":
+        addCppDefines( "_DEBUG" )
+    else:
+        addCppDefines( "NDEBUG" )
+    # TODO "WIN32" "_WINDOWS" "_USRDLL" "_WINDLL" "_UNICODE" "UNICODE"?
+
 
 """
-VS 2013 - cl debug
-/RTCc /GS     /analyze- /W3     /Zc:wchar_t /I"C:\OpenSource\nohtyP\Build\VS12.0\\..\.." /Zi /Gm  /Od      /sdl /Fd"Debug\vc120.pdb"   /fp:precise /D "yp_ENABLE_SHARED" /D "yp_BUILD_CORE" /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_USRDLL" /D "YPDLL_EXPORTS" /D "_WINDLL" /D "_UNICODE" /D "UNICODE" /errorReport:prompt     /WX /Zc:forScope /RTC1 /GR /Gd /Oy-     /MDd /Fa"Debug\"   /EHsc /nologo /Fo"Debug\"       /Fp"Debug\nohtyP.pch" 
-VS 2013 - cl release
-      /GS /GL /analyze- /W3 /Gy /Zc:wchar_t /I"C:\OpenSource\nohtyP\Build\VS12.0\\..\.." /Zi /Gm- /Ox /Ob2 /sdl /Fd"Release\vc120.pdb" /fp:precise /D "yp_ENABLE_SHARED" /D "yp_BUILD_CORE" /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_USRDLL" /D "YPDLL_EXPORTS" /D "_WINDLL" /D "_UNICODE" /D "UNICODE" /errorReport:prompt /GT /WX /Zc:forScope           /Gd /Oy- /Oi /MD  /Fa"Release\" /EHsc /nologo /Fo"Release\" /Ot /Fp"Release\nohtyP.pch" 
-
 VS 2013 - link debug
-/OUT:"C:\OpenSource\nohtyP\Build\VS12.0\Debug\nohtyP.dll"   /MANIFEST /PROFILE       /NXCOMPAT /PDB:"C:\OpenSource\nohtyP\Build\VS12.0\Debug\nohtyP.pdb"   /DYNAMICBASE /MAPINFO:EXPORTS "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" /LARGEADDRESSAWARE /IMPLIB:"C:\OpenSource\nohtyP\Build\VS12.0\Debug\nohtyP.lib"   /VERSION:"VersionHere" /DEBUG /DLL /MACHINE:X86 /WX                   /INCREMENTAL:NO /PGD:"C:\OpenSource\nohtyP\Build\VS12.0\Debug\nohtyP.pgd"   /SUBSYSTEM:WINDOWS /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /ManifestFile:"Debug\nohtyP.dll.intermediate.manifest"   /MAP":C:\OpenSource\nohtyP\Build\VS12.0\Debug\nohtyP.map"            /ERRORREPORT:PROMPT /NOLOGO /ASSEMBLYDEBUG /TLBID:1 
+/OUT:"C:\OpenSource\nohtyP\Build\VS12.0\Debug\nohtyP.dll"   /MANIFEST /PROFILE       /NXCOMPAT /PDB:"C:\OpenSource\nohtyP\Build\VS12.0\Debug\nohtyP.pdb"   /DYNAMICBASE /MAPINFO:EXPORTS "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" /LARGEADDRESSAWARE /IMPLIB:"C:\OpenSource\nohtyP\Build\VS12.0\Debug\nohtyP.lib"   /VERSION:"VersionHere" /DEBUG /DLL /MACHINE:X86 /WX                   /INCREMENTAL:NO /PGD:"C:\OpenSource\nohtyP\Build\VS12.0\Debug\nohtyP.pgd"   /SUBSYSTEM:WINDOWS /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /ManifestFile:"Debug\nohtyP.dll.intermediate.manifest"   /MAP":C:\OpenSource\nohtyP\Build\VS12.0\Debug\nohtyP.map"            /ERRORREPORT:PROMPT /NOLOGO /ASSEMBLYDEBUG /TLBID:1
 VS 2013 - link release
-/OUT:"C:\OpenSource\nohtyP\Build\VS12.0\Release\nohtyP.dll" /MANIFEST          /LTCG /NXCOMPAT /PDB:"C:\OpenSource\nohtyP\Build\VS12.0\Release\nohtyP.pdb" /DYNAMICBASE /MAPINFO:EXPORTS "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" /LARGEADDRESSAWARE /IMPLIB:"C:\OpenSource\nohtyP\Build\VS12.0\Release\nohtyP.lib" /VERSION:"VersionHere" /DEBUG /DLL /MACHINE:X86 /WX /OPT:REF /SAFESEH /INCREMENTAL:NO /PGD:"C:\OpenSource\nohtyP\Build\VS12.0\Release\nohtyP.pgd" /SUBSYSTEM:WINDOWS /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /ManifestFile:"Release\nohtyP.dll.intermediate.manifest" /MAP":C:\OpenSource\nohtyP\Build\VS12.0\Release\nohtyP.map" /OPT:ICF /ERRORREPORT:PROMPT /NOLOGO                /TLBID:1 
+/OUT:"C:\OpenSource\nohtyP\Build\VS12.0\Release\nohtyP.dll" /MANIFEST          /LTCG /NXCOMPAT /PDB:"C:\OpenSource\nohtyP\Build\VS12.0\Release\nohtyP.pdb" /DYNAMICBASE /MAPINFO:EXPORTS "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" /LARGEADDRESSAWARE /IMPLIB:"C:\OpenSource\nohtyP\Build\VS12.0\Release\nohtyP.lib" /VERSION:"VersionHere" /DEBUG /DLL /MACHINE:X86 /WX /OPT:REF /SAFESEH /INCREMENTAL:NO /PGD:"C:\OpenSource\nohtyP\Build\VS12.0\Release\nohtyP.pgd" /SUBSYSTEM:WINDOWS /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /ManifestFile:"Release\nohtyP.dll.intermediate.manifest" /MAP":C:\OpenSource\nohtyP\Build\VS12.0\Release\nohtyP.map" /OPT:ICF /ERRORREPORT:PROMPT /NOLOGO                /TLBID:1
 
-
+TODO Ensure /Zi or /Z7 gets added when PDB used, and that /Fd"Debug\vc120.pdb" isn't needed
 TODO /analyze? (enable /Wall when analyzing, then decide what to supress)
 
 SCons - cl
@@ -42,8 +73,7 @@ link /nologo /dll /out:Build\msvs_120\win32_amd64_release\nohtyP.dll /implib:Bui
 link /nologo /dll /out:Build\msvs_120\win32_x86_debug\nohtyP.dll     /implib:Build\msvs_120\win32_x86_debug\nohtyP.lib     Build\msvs_120\win32_x86_debug\nohtyP.obj
 link /nologo /dll /out:Build\msvs_120\win32_x86_release\nohtyP.dll   /implib:Build\msvs_120\win32_x86_release\nohtyP.lib   Build\msvs_120\win32_x86_release\nohtyP.obj
 
-TODO It must pick up a lot of information from the environment...find out what (on MSDN)
-
+TODO http://randomascii.wordpress.com/2012/07/22/more-adventures-in-failing-to-crash-properly/
 """
 
 
