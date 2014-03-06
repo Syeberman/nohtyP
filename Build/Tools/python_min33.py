9fb309f3fe6477ab2a5c0f105d77d146a88c68bd
Sye van der Veen <4551516+Syeberman@users.noreply.github.com>
2014-03-06 12:41:35 -0500

    Pick the best applicable version of Python, test against all possible builds with a single command ("testall"), ensure at lest Python 3.3 is used for yp_test, and ensure python_2 doesn't pick up a 3.x version

--- Build/Tools/python_min33.py
+++ Build/Tools/python_min33.py
@@ -1,6 +1,6 @@
 import python_common
 _generate, _exists = python_common.DefinePythonToolFunctions( 
-        range( 0x03000000, 0x04000000 ), "3.x" )
+        range( 0x03030000, 0x04000000 ), "3.3 (or better)" )
 # Define new functions so that we remain in any stack traces involving these functions
 def generate( *args, **kwargs ): return _generate( *args, **kwargs )
 def exists( *args, **kwargs ): return _exists( *args, **kwargs )
