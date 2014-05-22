52dd66160886363cf9592f9e6474d09905ee0660
Sye van der Veen <syeberman@gmail.com>
2014-05-21 21:39:06 -0400

    Recording the type of each parameter as well as its name

--- Lib/checkapi.py
+++ Lib/checkapi.py
@@ -4,17 +4,42 @@ Author: Sye van der Veen
 Date: May 19, 2014
 """
 
-import sys, re
+import sys, re, copy
 
 # FIXME remove these hard-coded paths
 CppPath = "../pycparser/utils/cpp"
 CppArgs = "-I../pycparser/utils/fake_libc_include"
 sys.path.append( "../pycparser" )
-from pycparser import c_parser, c_ast, parse_file
+from pycparser import c_parser, c_generator, c_ast, parse_file
 
 
 re_FuncName = re.compile( r"^(?P<root>yp_([ois]2[ois]_)?([a-z_]+|(asu?int\d+)|(asfloat\d+)))"
         "(?P<post>(CF?)?(LF?)?(NV?)?(KV?)?E?D?X?\d?)$" )
+Generator = c_generator.CGenerator( )
+
+
+class RemoveDeclNameVisitor( c_ast.NodeVisitor ):
+    def visit_TypeDecl( self, node ):
+        node.declname = ""
+        self.generic_visit( node )
+RemoveDeclName = RemoveDeclNameVisitor( ).visit
+def ConvertToAbstractType( declnode ):
+    abstract = copy.deepcopy( declnode )
+    RemoveDeclName( abstract )
+    return abstract
+
+
+class ypParameter:
+    @classmethod
+    def from_Decl( cls, declnode ):
+        declnode.show( )
+        self = cls( )
+        self.name = declnode.name
+        self.type = Generator.visit( ConvertToAbstractType( declnode ) )
+        return self
+    
+    def __str__( self ):
+        return "%r %r" % (self.type, self.name)
 
 
 class ypFunction:
@@ -25,14 +50,9 @@ class ypFunction:
 
         self = cls( )
         self.name = declnode.name
-
         self.params = []
         for pdeclnode in paramsnode.params:
-            if isinstance( pdeclnode, c_ast.EllipsisParam ):
-                self.params.append( "..." )
-            else:
-                self.params.append( pdeclnode.name )
-
+            self.params.append( ypParameter.from_Decl( pdeclnode ) )
         self._complete( )
 
     def _complete( self ):
@@ -41,7 +61,8 @@ class ypFunction:
         assert namematch is not None, self.name
         self.rootname = namematch.group( "root" )
         self.postfixes = namematch.group( "post" )
-        print( self.name, self.rootname, self.postfixes )
+        print( self.name, self.rootname, self.postfixes, *self.params )
+
 
 
 class ApiVisitor( c_ast.NodeVisitor ):
