014b0b682699eb0c690d9b37776f02962e8b1f4c
Sye van der Veen <syeberman@gmail.com>
2014-05-22 21:27:29 -0400

    Now parsing the whole file

--- Lib/checkapi.py
+++ Lib/checkapi.py
@@ -24,6 +24,7 @@ class RemoveDeclNameVisitor( c_ast.NodeVisitor ):
         self.generic_visit( node )
 RemoveDeclName = RemoveDeclNameVisitor( ).visit
 def ConvertToAbstractType( declnode ):
+    """Returns a deep copy of declnode converted to an abstract type (no declnames)."""
     abstract = copy.deepcopy( declnode )
     RemoveDeclName( abstract )
     return abstract
@@ -32,40 +33,49 @@ def ConvertToAbstractType( declnode ):
 class ypParameter:
     @classmethod
     def from_Decl( cls, declnode ):
-        declnode.show( )
+        """Creates a ypParameter based on the Decl node."""
         self = cls( )
-        self.name = declnode.name
-        self.type = Generator.visit( ConvertToAbstractType( declnode ) )
+        if isinstance( declnode, c_ast.EllipsisParam ):
+            self.name = self.type = "..."
+        else:
+            self.name = declnode.name
+            self.type = Generator.visit( ConvertToAbstractType( declnode ) )
         return self
-    
+
     def __str__( self ):
-        return "%r %r" % (self.type, self.name)
+        return "%r: %r" % (self.name, self.type)
 
 
 class ypFunction:
     @classmethod
     def from_Decl( cls, declnode ):
+        """Creates a ypFuncion based on the Decl node."""
         paramsnode = declnode.type.args
         returnnode = declnode.type.type
 
         self = cls( )
         self.name = declnode.name
         self.params = []
-        for pdeclnode in paramsnode.params:
-            self.params.append( ypParameter.from_Decl( pdeclnode ) )
+        if len( paramsnode.params ) > 1 or getattr( paramsnode.params[0], "name", "" ) is not None:
+            for pdeclnode in paramsnode.params:
+                self.params.append( ypParameter.from_Decl( pdeclnode ) )
         self._complete( )
 
     def _complete( self ):
-        """Common initialization"""
+        """Common initialization for ypFunction."""
         namematch = re_FuncName.match( self.name )
         assert namematch is not None, self.name
         self.rootname = namematch.group( "root" )
         self.postfixes = namematch.group( "post" )
-        print( self.name, self.rootname, self.postfixes, *self.params )
+        print( self )
 
+    def __str__( self ):
+        return "%s %s %r (%s)" % (self.name, self.rootname, self.postfixes,
+                ", ".join( str( x ) for x in self.params ))
 
 
 class ApiVisitor( c_ast.NodeVisitor ):
+    """Collects information from nohtyP.h."""
     def __init__( self ):
         self.entities = []
 
@@ -75,7 +85,19 @@ class ApiVisitor( c_ast.NodeVisitor ):
             # node.show( )
 
 
+# TODO Checks to implement
+#   - ellipsis preceeded by int n
+#   - ellipsis is in an N or K (va_list is NV or KV)
+#   - exc is always ypObject ** and used in C, F, L, and E
+#   - the count equals the actual arg count (minus exc)
+#   - C is used iff it contains a C type (an int?)
+#   - F is used iff it contains a float
+#   - L doesn't use ypObject *
+#   - report the functions that belong to the same group, and which is the unadorned version
+
+
 def CheckApi( filename ):
+    """Reports potential problems in nohtyP.h (and related files)."""
     # TODO improve cpp_args
     ast = parse_file( filename, use_cpp=True, cpp_path=CppPath, cpp_args=CppArgs )
     v = ApiVisitor( )
