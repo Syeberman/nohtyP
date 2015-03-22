c3295c1daf187a53c136d9f27ec0b7e27d24215f
Sye van der Veen <syeberman@gmail.com>
2015-03-22 14:50:42 -0400

    Add support for GCC 4.6

---
+++ Build/Tools/gcc_46.py
@@ -0,0 +1,6 @@
+import gcc_common
+_generate, _exists = gcc_common.DefineGCCToolFunctions( 4.6, major=4, minor=6 )
+# Define new functions so that we remain in any stack traces involving these functions
+def generate( *args, **kwargs ): return _generate( *args, **kwargs )
+def exists( *args, **kwargs ): return _exists( *args, **kwargs )
+
