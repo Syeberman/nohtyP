d5ada1000f65e06abd6bf2005b901ef5d8afd659
Sye van der Veen <4551516+Syeberman@users.noreply.github.com>
2018-09-30 17:29:07 -0400

    Add gcc 7.3 and 8.1, and use latest MSVS 2017 version
    However, I think I need to update SCons to autodetect MSVS 2017.

---
+++ Build/site_scons/site_tools/msvs_158.py
@@ -0,0 +1,13 @@
+import msvs_common
+# FIXME It seems MS is using the minor version as more of a build number now, so we should just
+# accept any "version 15" Visual Studio we find.
+_generate, _exists = msvs_common.DefineMSVSToolFunctions(15.8, ("15.8", ))
+
+
+# Define new functions so that we remain in any stack traces involving these functions
+def generate(*args, **kwargs):
+    return _generate(*args, **kwargs)
+
+
+def exists(*args, **kwargs):
+    return _exists(*args, **kwargs)
