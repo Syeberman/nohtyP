9d8ceada0454398791b5e0615b03d66488b67d4a
Sye van der Veen <syeberman@gmail.com>
2022-03-05 12:06:22 -0500

    test_unicodedata passing

--- Lib/yp_test/test_unicodedata.py
+++ Lib/yp_test/test_unicodedata.py
@@ -318,6 +318,7 @@ class UnicodeMiscTest(UnicodeDatabaseTest):
                 self.assertEqual(len(lines), 1,
                                  r"\u%.4x should not be a linebreak" % i)
 
+@yp_unittest.skip_str_unicode_db
 class NormalizationTest(yp_unittest.TestCase):
     @staticmethod
     def check_version(testfile):
