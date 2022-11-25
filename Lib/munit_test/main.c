f8d51c7f5f5a8df14d052202736e9fd0226e5d5d
Sye van der Veen <syeberman@gmail.com>
2022-11-25 17:20:32 -0500

    Get munit building

--- Lib/munit_test/main.c
+++ Lib/munit_test/main.c
@@ -4,7 +4,6 @@
 
 // TODO Write a pycparser script that ensures all test functions lead back to here. Also to ensure
 // the suite names match the directory/file names.
-#if 0
 static const MunitSuite test_suite = {
         "",                      // prefix
         NULL,                    // tests
@@ -12,7 +11,6 @@ static const MunitSuite test_suite = {
         1,                       // iterations
         MUNIT_SUITE_OPTION_NONE  // options
 };
-#endif
 
 int main(int argc, char **argv)
 {
@@ -21,10 +19,8 @@ int main(int argc, char **argv)
         yp_initialize(&args);
     }
 
-#if 0
     {
         void *user_data = NULL;
         return munit_suite_main(&test_suite, user_data, argc, argv);
     }
-#endif
 }
