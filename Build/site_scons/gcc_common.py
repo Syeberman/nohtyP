a2c9372337783c87b67c3feb8ac65b2cf6156b1e
Sye van der Veen <svanderveen@coursera.org>
2022-11-27 17:05:16 -0500

    Fix MacOS build, which doesn't have a separate import library

--- Build/site_scons/gcc_common.py
+++ Build/site_scons/gcc_common.py
@@ -137,7 +137,6 @@ def _updateCcEmitters(env):
 
 def _linkEmitter(target, source, env):
     t_base, t_ext = os.path.splitext(target[0].path)
-    # TODO Remove these asserts once we've gotten this right
     assert t_ext in (".dll", ".dylib", ".exe", ".so", ".", ""), target[0].path
     for ext in (".map", ):
         env.Clean(target[0], t_base + ext)
